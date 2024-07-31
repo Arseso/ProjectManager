c26df0ffc3cb6a930388e4c8ccab3e063d21d0d4
Artem Sokolov <arseso.main@gmail.com>
2024-07-31 22:24:40 +0300

    Text input editions

--- src/preprocessing/ca.py
+++ src/preprocessing/ca.py
@@ -24,7 +24,7 @@ def _preprocessing_pipeline(text: list[str]) -> list[str]:
     return result
 
 
-def _find_to(text: list[str]) -> str | None:
+def _find_to(text: list[str]) -> tuple[str | None, list[str]]:
     """
     :param text: list of lines from email
     :return: recipient's address, None if didn't find one
@@ -33,34 +33,34 @@ def _find_to(text: list[str]) -> str | None:
 
     if match:
         email = match.group(0)
-        return email
+        return email, text[1:]
     else:
-        return None
+        return None, text
 
 
-def _find_subject(text: list[str]) -> str | None:
+def _find_subject(text: list[str]) -> tuple[str | None, list[str]]:
     """
     :param text: list of lines from email
     :return: message subject string, None if didn't find one
     """
     try:
-        subj = text[1].split(": ", 1)[1]
-        return subj
+        subj = text[0].split(": ", 1)[1]
+        return subj, text[:1]
     except IndexError:
-        return None
+        return None, text
 
 
-def _find_name_from_greeting(text: list[str]) -> str | None:
+def _find_name_from_greeting(text: list[str]) -> tuple[str | None, list[str]]:
     """
     :param text: list of lines from email
     :return: recipient's name from greeting, None if didn't find one
     """
     pattern = rf"^({PATTERN_GREETING})\s*(.*)"
-    match = re.match(pattern, text[2])
+    match = re.match(pattern, text[0])
     if match:
-        return match.group(2).replace(",", "")
+        return match.group(2).replace(",", ""), text[1:]
     else:
-        return None
+        return None, text
 
 
 def _find_body(text: list[str]) -> list[str] | None:
@@ -68,8 +68,7 @@ def _find_body(text: list[str]) -> list[str] | None:
     :param text: list of lines from email
     :return: email body as list of lines, None if didn't find one
     """
-    body = text[3:-2]
-    return body if body else None
+    return text if text else None
 
 
 def _find_sign(text: list[str]) -> str | None:
@@ -79,9 +78,9 @@ def _find_sign(text: list[str]) -> str | None:
     """
 
     if re.match(PATTERN_SIGN, text[-1]):
-        return text[-1]
+        return text[-1], text[:-2]
     else:
-        return None
+        return None, text
 
 
 def text_to_model_ca(text: list[str]) -> TextCA:
@@ -89,12 +88,19 @@ def text_to_model_ca(text: list[str]) -> TextCA:
     :param text: list of lines from email
     :return: model from text for analyze CA metrics
     """
+    
     text = _preprocessing_pipeline(text)
+    _to, text = _find_to(text)
+    _subj, text = _find_subject(text)
+    _sign, text = _find_sign(text)
+    _name, text = _find_name_from_greeting(text)
+    _body = _find_body(text)
     model = TextCA(
-        _find_to(text),
-        _find_subject(text),
-        _find_name_from_greeting(text),
-        _find_body(text),
-        _find_sign(text)
+        to =_to,
+        subject = _subj,
+        greeting_name = _name,
+        sign = _sign,
+        body = _body
+        
     )
     return model
